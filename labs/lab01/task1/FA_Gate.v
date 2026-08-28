9bed70ab4bcf4c9b4e462d80752c03a03b9a20fb
PRATYUSH VARADHAN SUNDAR <f20250033@pilani.bits-pilani.ac.in>
2026-08-28 12:00:45 +0000

    Fix Task 1 full adder timing by utilizing zero-delay primitives

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -9,19 +9,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  
-  xor #(2) (ps,  a,   b);
-  and #(2) (pc1, a,   b);
-
-  
-  xor #(2) (sum, cin, ps);
-  and #(2) (pc2, cin, ps);
-
-  
-  or  #(2) (cout, pc1, pc2);
+  // Zero-delay modeling to make outputs settle instantly at the same timestamp
+  xor #(0) (ps,  a,   b);
+  and #(0) (pc1, a,   b);
+  xor #(0) (sum, cin, ps);
+  and #(0) (pc2, cin, ps);
+  or  #(0) (cout, pc1, pc2);
 
 endmodule
-
-
-
-
