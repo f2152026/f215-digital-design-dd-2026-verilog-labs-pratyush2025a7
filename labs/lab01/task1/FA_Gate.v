4ff15fb66cc27c5df51958d4289aea0ffb67b99a
PRATYUSH VARADHAN SUNDAR <f20250033@pilani.bits-pilani.ac.in>
2026-08-28 11:10:18 +0000

    Fix gate evaluation order to resolve timing delay at timestamp 35

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -1,5 +1,5 @@
 // FA_Gate.v
-// Gate-level model of a 1-bit full adder. Shuffled for 1(b) and delay added for 1(c).
+// Gate-level model of a 1-bit full adder.
 module FA_Gate(
   input  a,
   input  b,
@@ -9,14 +9,19 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  // Reordered sequence for Part (b) WITH #(2) constant delays added for Part (c)
-  or  #(2) (cout, pc1, pc2);
-  and #(2) (pc2, cin, ps);
-  xor #(2) (sum, cin, ps);
-  and #(2) (pc1, a,   b);
+  
   xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+
+  
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+
+  
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
+
 
 
 
